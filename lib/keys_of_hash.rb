565ecec5d6e65cf6fbafba161b80d25929d60503
yong liang <yongliang24@gmail.com>
2019-01-23 06:34:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,11 @@
 class Hash
   def keys_of(*arguments)
-    keysarray = []
-    arguments.each do |arg|
-    self.each do |key, val|
-if val == arg
-keysarray << key
-end
-end
-end
-
- return keysarray
+    animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
   end
 end
